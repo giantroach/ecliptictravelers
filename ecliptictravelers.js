efe80089fe33ec1b97d8eebc163451349e6ff67d
Tomoki Motohashi <tomoki.motohashi@gmail.com>
2022-01-19 21:28:01 +0900

    fix break flag that wrongly set to be true

--- ecliptictravelers.js
+++ ecliptictravelers.js
@@ -43,7 +43,7 @@ const cardDef = [
     {id: 21, time: 'd', location: 'f', river: 'b', break: false},
     {id: 22, time: 'tn', location: '', river: 'r', break: false},
     {id: 23, time: 'n', location: 'c', river: 'r', break: false},
-    {id: 24, time: 'mn', location: 'f', river: '', break: true},
+    {id: 24, time: 'mn', location: 'f', river: '', break: false},
 
     {id: 25, time: 'n', location: 'f', river: 'b', break: false},
     {id: 26, time: 'td', location: '', river: 'r', break: false},
